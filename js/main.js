8c5a213751255cefc7970696001701d815cf21a9
MakaoPoo <miyamasa0421@gmail.com>
2020-04-01 01:03:55 +0900

    口の動きを滑らかにした。

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 const video = document.getElementById("video");
-const canvas = document.getElementById("canvas");
+// const canvas = document.getElementById("canvas");
 let app;
 let fooko = null;
 
 $(async function() {
   app = new PIXI.Application({
     view: document.getElementById("fooko"),
-    width: 1000,
-    height: 1000,
+    width: 680,
+    height: 680,
     transparent: true,
   });
 
-  app.stop();
   app.loader.add('fooko', 'resource/fooko.json')
   .load(onAssetsLoaded);
 
@@ -35,126 +34,135 @@ $(async function() {
 });
 
 const onPlay = () => {
-  const message = document.getElementById('message')
-  const inputSize = 512; // 認識対象のサイズ
-  const scoreThreshold = 0.3; // 数値が高いほど精度が高くなる（〜0.9）
-  // (2)オプション設定
+  const inputSize = 160; // 認識対象のサイズ
+  const scoreThreshold = 0.2; // 数値が高いほど精度が高くなる（〜0.9）
   const options = new faceapi.TinyFaceDetectorOptions({
     inputSize,
     scoreThreshold
   })
-  const detectInterval = setInterval(async () => {
-    // (3)顔認識処理
+
+  setInterval(async () => {
     const result = await faceapi.detectSingleFace(
       video,
       options
     )
-    .withFaceLandmarks()
+    .withFaceLandmarks();
 
     if (result) {
-      const resizedDetections = faceapi.resizeResults(result, {
-        width: 400,
-        height: 400
-      });
+      const mouthPos = result.landmarks.getMouth();
+      const nosePos = result.landmarks.getNose()
+      const outlinePos = result.landmarks.getJawOutline()
 
-      const mouthPos = resizedDetections.landmarks.getMouth();
-      const outlinePos = resizedDetections.landmarks.getJawOutline()
-      // console.log(outlinePos);
-
-      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-      faceapi.draw.drawDetections(canvas, resizedDetections);
-      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+      // faceapi.draw.drawDetections(canvas, resizedDetections);
+      // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
       // drawDot(canvas, mouthPos[12]);
       // drawDot(canvas, mouthPos[16]);
       // drawDot(canvas, mouthPos[14]);
       // drawDot(canvas, mouthPos[18]);
       // drawDot(canvas, outlinePos[0]);
       // drawDot(canvas, outlinePos[8]);
-      // drawDot(canvas, outlinePos[16]);
+      // drawDot(canvas, getVecCenter(nosePos[0], getVecCenter(outlinePos[0], outlinePos[16])));
+      // drawDot(canvas, outlinePos[8]);
 
       const mouthHLength = getVecLength(mouthPos[12], mouthPos[16]);
       const mouthVLength = getVecLength(mouthPos[14], mouthPos[18]);
       const faceHLength = getVecLength(outlinePos[0], outlinePos[16]);
+      const faceVLength = getVecLength(outlinePos[0], getVecCenter(nosePos[0], getVecCenter(outlinePos[0], outlinePos[16])));
+      const noseFaceLength = getVecLength(nosePos[0], getVecCenter(outlinePos[0], outlinePos[16]));
 
-      let mouthVecRate = ((mouthHLength / mouthVLength) - 0.5) ** 1.5;
+      const mouthVRate = (mouthVLength / faceVLength) * 100;
+      let mouthHRate = (mouthHLength / faceHLength) * 100;
 
-      if(mouthVecRate > 2) {
-        mouthVecRate = 2;
-      }
-      if(mouthVecRate < 0.5) {
-        mouthVecRate = 0.5;
-      }
+      const mouthVVal = getRateVal(mouthVRate, 2, 16) + getRateVal(mouthVRate, 16, 22) * 0.9 - getRateVal(mouthHRate, 26, 32) * 0.3;
+      const mouthHVal = 0.5 + getRateVal(mouthHRate, 26, 29) * 0.6 + getRateVal(mouthHRate, 29, 35) * 1.0 + getRateVal(mouthVRate, 14, 22) * 0.2;
+
+      // console.log("V: " + mouthVRate);
+      // console.log("H: " + mouthHRate);
+      // console.log("N: " + Math.abs(nosePos[0].x - getVecCenter(outlinePos[0], outlinePos[16]).x));
+      // console.log("");
 
       if(fooko) {
         const mouthBone = fooko.skeleton.findBone("mouth");
-        let scaleY = ((120/32) * (mouthHLength / faceHLength))**2.5;
-        if(scaleY > 2) {
-          scaleY = 2;
-        }
-        histData.addHist("mouthScaleY", scaleY, 5);
-        mouthBone.scaleY = histData.getHistAve("mouthScaleY");
-        if(mouthVLength < 3) {
+        let scaleX = mouthHVal;
+
+        histData.addHist("mouthScaleX", scaleX, 8);
+        mouthBone.scaleY = histData.getHistAve("mouthScaleX");
+
+        let scaleY = mouthVVal;
+        histData.addHist("mouthscaleY", scaleY, 8);
+        scaleY = histData.getHistAve("mouthscaleY");
+
+        if(scaleY < 0.1) {
           fooko.skeleton.setAttachment("mouth", "mouth-close");
-          mouthBone.scaleX = 1;
+          scaleY = 1;
         } else {
           fooko.skeleton.setAttachment("mouth", "mouth-open");
-          let scaleX = (mouthVLength-3) / 11;
-          if(scaleX > 2) {
-            scaleX = 2;
-          }
-          histData.addHist("mouthscaleX", scaleX, 5);
-          mouthBone.scaleX = histData.getHistAve("mouthscaleX");
         }
+        mouthBone.scaleX = scaleY;
 
         const faceRot = getVecRot(outlinePos[0], outlinePos[16]);
         const faceBone = fooko.skeleton.findBone("face");
-        let deg = getDeg(faceRot);
-        if(deg > 20) { deg = 20; }
-        if(deg < -20) { deg = -20; }
+        let faceDeg = getDeg(faceRot);
+        if(faceDeg > 15) { faceDeg = 15; }
+        if(faceDeg < -15) { faceDeg = -15; }
 
-        histData.addHist("faceDeg", deg, 15);
+        histData.addHist("faceDeg", faceDeg, 15);
         faceBone.rotation = histData.getHistAve("faceDeg");
+
+        let bone = fooko.skeleton.findBone("back-hair");
+        let deg = (bone.getWorldRotationX() - 90) * 0.02;
+        deg -= degSpeed * 0.1;
+        degSpeed += deg;
+
+        bone = fooko.skeleton.findBone("back-hair");
+        bone.rotation += degSpeed;
+
+        bone = fooko.skeleton.findBone("right-hair");
+        bone.rotation += degSpeed;
+
+        bone = fooko.skeleton.findBone("left-hair");
+        bone.rotation += degSpeed;
+
+        bone = fooko.skeleton.findBone("center-hair");
+        bone.rotation += degSpeed;
+
+        time += 2;
+
+        const eyeTime = time % 400;
+        const eyeScale = (eyeTime <= 14)? ((eyeTime-7)/7)**2 : 1;
+
+        bone = fooko.skeleton.findBone("left-eye");
+        bone.scaleX = eyeScale;
+        bone = fooko.skeleton.findBone("right-eye");
+        bone.scaleX = eyeScale;
+
       }
 
       // message.textContent = "認識されてます"
     } else {
       // message.textContent = "認識されていません"
     }
-  }, 16);
+  }, 10);
 
-  app.ticker.add(() => {
-    time += 2;
+  renderLoop();
+}
 
-    let deg = Math.cos(getRad(time)) * 0.08;
+const renderLoop = function() {
+  fooko.update();
 
-    let bone;
+  requestAnimationFrame(renderLoop);
+}
 
-    bone = fooko.skeleton.findBone("back-hair");
-    deg += (bone.getWorldRotationX() - 90) * 0.02;
-    deg -= degSpeed * 0.1;
+const getRateVal = function(rate, min, max) {
+  if(rate <= min) {
+    return 0;
+  }
+  if(rate >= max) {
+    return 1;
+  }
 
-    degSpeed += deg;
-    bone.rotation += degSpeed;
-
-    bone = fooko.skeleton.findBone("right-hair");
-    bone.rotation += degSpeed;
-
-    bone = fooko.skeleton.findBone("left-hair");
-    bone.rotation += degSpeed;
-
-    bone = fooko.skeleton.findBone("center-hair");
-    bone.rotation += degSpeed;
-
-    const eyeTime = time % 400;
-    const eyeScale = (eyeTime <= 14)? ((eyeTime-7)/7)**2 : 1;
-
-    bone = fooko.skeleton.findBone("left-eye");
-    bone.scaleX = eyeScale;
-    bone = fooko.skeleton.findBone("right-eye");
-    bone.scaleX = eyeScale;
-
-    fooko.update(0.016666); // HARDCODED FRAMERATE!
-  });
+  return (rate - min) / (max - min);
 }
 
 let degSpeed = 0;
@@ -208,6 +216,14 @@ const getVecLength = function(pos1, pos2) {
   return length;
 }
 
+const getVecCenter = function(pos1, pos2) {
+  const vec = {
+    x: (pos2.x + pos1.x) / 2,
+    y: (pos2.y + pos1.y) / 2,
+  }
+  return vec;
+}
+
 const getVecRot = function(pos1, pos2) {
   const rot = Math.atan2(pos2.y - pos1.y, pos2.x - pos1.x);
   return rot
@@ -215,17 +231,15 @@ const getVecRot = function(pos1, pos2) {
 
 function onAssetsLoaded(loader, res) {
   fooko = new PIXI.spine.Spine(res.fooko.spineData);
-  fooko.autoUpdate = true;
+  fooko.autoUpdate = false;
 
-  const scale = 1.5;
-  fooko.scale.set(scale, scale);
   fooko.position.set(
-    500,
-    1000,
+    340,
+    680,
   );
 
   app.stage.addChild(fooko);
-  app.start();
+  fooko.update(0);
 }
 
 let time = 0;
